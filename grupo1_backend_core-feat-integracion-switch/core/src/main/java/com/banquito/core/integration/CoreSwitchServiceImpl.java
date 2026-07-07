75b30b0047852f41fe6792b8ffe4ccba30eb1092
Anahy Herrera <aeherrera16@espe.edu.ec>
2026-07-07 18:28:58 -0500

    fix: revertir rechazo de comision por saldo insuficiente
    Segun el requisito, el sobregiro SI esta permitido en el cobro de comision/IVA (unico punto donde el banco puede quedar en descubierto), no en los pagos individuales de nomina. Los pagos individuales ya rechazaban correctamente por saldo insuficiente sin abortar el archivo.

--- grupo1_backend_core-feat-integracion-switch/core/src/main/java/com/banquito/core/integration/CoreSwitchServiceImpl.java
+++ grupo1_backend_core-feat-integracion-switch/core/src/main/java/com/banquito/core/integration/CoreSwitchServiceImpl.java
@@ -9,7 +9,6 @@ import com.banquito.core.enums.CustomerSubtypeStatusEnum;
 import com.banquito.core.enums.CustomerTypeEnum;
 import com.banquito.core.enums.MovementTypeEnum;
 import com.banquito.core.enums.TransactionStatusEnum;
-import com.banquito.core.exception.InsufficientBalanceException;
 import com.banquito.core.model.Account;
 import com.banquito.core.model.AccountTransaction;
 import com.banquito.core.model.Customer;
@@ -182,16 +181,17 @@ public class CoreSwitchServiceImpl implements CoreSwitchService {
 
             TransactionSubtype subtype = getActiveSubtype("COMISION");
 
-            if (companyAccount.getAvailableBalance().compareTo(totalAmount) < 0) {
-                throw new InsufficientBalanceException(companyAccountNumber);
-            }
-
             companyAccount.setAvailableBalance(companyAccount.getAvailableBalance().subtract(totalAmount));
             companyAccount.setAccountingBalance(companyAccount.getAccountingBalance().subtract(totalAmount));
             companyAccount.setLastUpdate(LocalDateTime.now());
             accountRepository.save(companyAccount);
             registerCompanyMovement(companyAccount, totalAmount, uuid, subtype,
                     "Global debit for mass payment service");
+
+            if (companyAccount.getAccountingBalance().compareTo(BigDecimal.ZERO) < 0) {
+                log.warn("Company account {} entered overdraft after commission debit. Resulting balance: {}",
+                        companyAccountNumber, companyAccount.getAccountingBalance());
+            }
 
             creditInstitutionalAccount(MASS_SERVICE_INCOME_ACCOUNT, commissionSubtotal);
             creditInstitutionalAccount(VAT_PAYABLE_ACCOUNT, vatAmount);
